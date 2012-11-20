9448820df4fdcfe42a397f161157e41402b59735
fbushell <frank@gocirro.com>
2012-11-20 10:09:53 -0700

    removed the rest of the jquery and css files #4 also commented out normalize for now as twitter bootstrap uses a modified version already

--- template.php
+++ template.php
@@ -1,6 +1,11 @@
 <?php
 
+/**
+ * function that unsets all Drupal added css
+ */
 function clim_css_alter(&$css) {
+  
+  // Create exclude array to pass
   $exclude = array(
     'misc/vertical-tabs.css' => FALSE,
     'modules/block/block.css' => FALSE,
@@ -33,15 +38,35 @@ function clim_css_alter(&$css) {
     'modules/update/update.css' => FALSE,
     'modules/user/user.css' => FALSE,
   );
+
   $css = array_diff_key($css, $exclude);
+
 }
 
+/**
+ * function that unsets all Drupal added js and updates jQuery
+ */
 function clim_js_alter(&$javascript) {
 
-	$jquery_new = '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';
+  $jquery_new = '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';
+  $jquery_version = '1.8.3';
+  $jquery_newest = '//code.jquery.com/jquery-latest.min.js';
 
-  // Swap out jQuery to use an updated version of the library.
-  $javascript['misc/jquery.js']['data'] = $jquery_new;
+  // Swap out jQuery to use an updated version and move to footer.
+  $javascript['misc/jquery.js']['data']   = $jquery_new;
+  $javascript['misc/jquery.js']['version']   = $jquery_version;
+  $javascript['misc/jquery.js']['scope']  = 'header';
+  $javascript['misc/jquery.js']['weight'] = 0;
 
+  // Unset the rest of Drupal added js
+  unset($javascript['settings']);
+  unset($javascript['misc/jquery.once.js']);
+  unset($javascript['misc/drupal.js']);
+  unset($javascript['sites/all/modules/devel/devel_krumo_path.js']);
+  unset($javascript['misc/jquery.cookie.js']);
+  unset($javascript['modules/toolbar/toolbar.js']);
+
+  // Check out $javascript 
+  //dsm($javascript);
 
 }
